d8aa1ad19a1a9a55f91d95d5fabb6006b5f6d718
Nicolas <nicolas_verkens@hotmail.com>
2023-09-21 17:58:27 +0200

    Print + programme jeu

---
+++ src/Combatnaval/Program.java
@@ -0,0 +1,23 @@
+package Combatnaval;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello World !\n");
+        System.out.println();
+
+        // Déclarations et initialisation de J1_Points
+        var J1_Points = 0f;
+        /*
+        Idem pour J2_Points
+        Nombre de joueur 2
+         */
+        var J2_Points = J1_Points;
+
+        // Déclaration de constante
+
+        final var MAX=POINTS = 5 + 4 + 3 + 2 + 2 + 1;
+
+        // Création d'un tableau pour la grille d'attaque
+        var GrilleAttaque = new char[100];
+    }
+}
